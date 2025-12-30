69b0f31350799cf447efe41c08eb8b2e630aa1a4
alli <alliatpr@gmail.com>
2025-12-30 02:42:17 -0500

    script.js

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ function randomPost() {
   const posts = document.querySelectorAll(".post");
   const random = Math.floor(Math.random() * posts.length);
   posts[random].scrollIntoView({ behavior: "smooth" });
+
+  // fun shake effect
+  const button = document.querySelector(".sidebar button");
+  button.classList.add("shake");
+  setTimeout(() => button.classList.remove("shake"), 500);
 }
